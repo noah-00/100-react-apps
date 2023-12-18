29a128e479ef97042b96ebe60dc3168a88dbc853
noah-00 <t71.ishizaka.naoya@gmail.com>
2023-12-18 21:46:30 +0900

    ✨ init

---
+++ 01_hello_world/src/components/HelloWorld.tsx
@@ -0,0 +1,5 @@
+export const HelloWorld = () => {
+  return (
+    <div className="h-screen flex justify-center items-center font-bold text-9xl">Hello World</div>
+  );
+};
---
+++ README.md
@@ -0,0 +1,1 @@
+# 100-react-apps
